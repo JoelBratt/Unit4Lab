dc53e49b6b96cabc6172f95881a7d631a7630622
Joel Bratt <jbratt1@student.cscc.edu>
2026-06-24 21:34:54 -0400

    coin.py determines heads or tails

--- coin.py
+++ coin.py
@@ -1,6 +1,21 @@
 """
-
-
-
-
+coin.py
+Joel Bratt
+make a coin class for use in the main py
+6/24/2026
 """
+import random
+class Coin:
+    def __init__(self):
+        self.__sideup = 'Heads'
+        self.toss()
+
+    def toss(self):
+
+        if random.randint(0, 1)==0:
+            self.__sideup = 'Heads'
+        else:
+            self.__sideup = 'Tails'
+    
+    def get_side(self):
+        return self.__sideup
